dfdc07e958833384e97bb3a3e3345a7af1c9aee8
Hilda Enyioko <enyiokohilda@gmail.com>
2025-12-17 14:36:35 +0100

    add graphql types

--- crm/schema.py
+++ crm/schema.py
@@ -12,3 +12,13 @@ class CustomerType(DjangoObjectType):
     class Meta:
         model = Customer
         fields = "__all__"
+        
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = "__all__"
+        
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
+        fields = "__all__"
