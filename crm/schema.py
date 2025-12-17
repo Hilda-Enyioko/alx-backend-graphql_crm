8778bae44222ae9f106cce83e7530a6fc008cfac
Hilda Enyioko <enyiokohilda@gmail.com>
2025-12-17 14:50:19 +0100

    add basic query

--- crm/schema.py
+++ crm/schema.py
@@ -22,3 +22,22 @@ class OrderType(DjangoObjectType):
     class Meta:
         model = Order
         fields = "__all__"
+        
+
+# Basic Query to determine the data that can be read from the GraphQL endpoint
+class Query(graphene.ObjectType):
+    # Every read operation starts from the query
+    # If a dataset is not defined here, the client cannot fetch it
+    customers = graphene.List(CustomerType)             # List all CustomerTypes
+    products = graphene.List(ProductType)
+    orders = graphene.List(OrderType)
+    
+    # Next, we use resolvers tell GraphQL where to get this data from
+    def resolve_customers(root, info):
+        return Customer.objects.all()
+    
+    def resolve_products(root, info):
+        return Product.objects.all()
+    
+    def resolve_orders(root, info):
+        return Order.object.all()
