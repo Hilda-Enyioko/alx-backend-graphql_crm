0a7b1a086d2dc41e26a9501a8400d4af2ea1d8a7
Hilda Enyioko <enyiokohilda@gmail.com>
2026-01-10 16:55:15 +0100

    edited file

--- crm/schema.py
+++ crm/schema.py
@@ -5,7 +5,7 @@ from django.db import transaction
 from django.utils import timezone
 from graphene_django.filter import DjangoFilterConnectionField
 from .filters import CustomerFilter, ProductFilter, OrderFilter
-from .models import Customer, Product, Order
+from crm.models import Customer, Product, Order
 
 
 # Create GraphQL objecttypes for safe querying
